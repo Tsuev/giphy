31a287acb3cc2fab11afb87c89e0c19778a35f21
Tengreyt <halid.taramov20004@gmail.com>
2024-06-14 01:12:34 +0300

    commit 1.1

--- assets/scripts/services.js
+++ assets/scripts/services.js
@@ -4,13 +4,13 @@ const KEY = '6yb0lbVAwM4Nm2wmvfEu5C8MUL6yXIwD';
 // https://api.giphy.com/v1/gifs/search?api_key=6yb0lbVAwM4Nm2wmvfEu5C8MUL6yXIwD&q=ronaldo&limit=10
 
 async function getTrendingGifs(){
-    const response = await fetch(`${URL}/trending?api_key=${KEY}&limit=10`);
+    const response = await fetch(`${URL}/trending?api_key=${KEY}&limit=15`);
     const { data } = await response.json();
     return data 
 }
 
 async function serchGifs(request){
-    const response = await fetch(`${URL}/search?api_key=${KEY}&q=${request}&limit=10`)
+    const response = await fetch(`${URL}/search?api_key=${KEY}&q=${request}&limit=15`)
     const { data } = await response.json(); 
     return data
 }
